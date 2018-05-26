37f50ad0ccc93b362cc60f05a5493db07b0e36bd
Paul Nagle <paulnagle@gmail.com>
2018-05-26 19:03:53 +0100

    Enter address search

--- src/pages/address-search/address-search-component/address-search.component.ts
+++ src/pages/address-search/address-search-component/address-search.component.ts
@@ -1,7 +1,19 @@
-import { Component }  from '@angular/core';
-import { Config }     from '../../../app/app.config';
-import { Storage }    from '@ionic/storage';
+import { Component, ViewChild }       from '@angular/core';
+import { LoadingController }          from 'ionic-angular';
+import { Config }                     from '../../../app/app.config';
+import { Storage }                    from '@ionic/storage';
+import { ToastController }            from 'ionic-angular';
+import { Geolocation }                from '@ionic-native/geolocation';
+import { MouseEvent,
+         LatLngLiteral,
+         LatLngBounds,
+         LatLng,
+         AgmCircle,
+         MapsAPILoader  }             from '@agm/core';
+import { MeetingListProvider }        from '../../../providers/meeting-list/meeting-list';
+import { TranslateService }           from '@ngx-translate/core';
 
+declare const google: any;
 
 @Component({
   selector: 'page-address-search',
@@ -9,24 +21,190 @@ import { Storage }    from '@ionic/storage';
 })
 export class AddressSearchComponent {
 
-  autoRadius;
+  timeDisplay        : string  = "";
+  meetingList        : any     = [];
+  loader                       = null;
+  zoom               : number  = 8;
+  mapLatitude        : any     =  51.899 ;
+  mapLongitude       : any     = -8.474 ;
+  autoRadius         : any;
+  map                : any     = null ;
+  mapBounds          : LatLngBounds;
+  myLatLng           : LatLng;
+  circleRadiusMeters : number  = 0 ;
+  formattedAddress   : string;
 
-  constructor(	private storage     : Storage ,
-                private config      : Config  ) {
+  @ViewChild('circle', {read: AgmCircle}) circle: AgmCircle;
 
-    this.storage.get('searchRange')
-    .then(searchValue => {
-        if(searchValue) {
-          console.log("Setting radius to ", searchValue);
-          this.autoRadius = searchValue;
+  constructor(private MeetingListProvider : MeetingListProvider,
+              public  loadingCtrl         : LoadingController,
+              private geolocation         : Geolocation,
+              private toastCtrl           : ToastController,
+              private storage             : Storage,
+              private translate           : TranslateService,
+              private MapsAPIloader       : MapsAPILoader  ) {
+
+     console.log("AddressSearchComponent: constructor");
+  }
+
+
+  mapReady(event: any) {
+    console.log("mapReady : event");
+    this.map = event;
+
+    this.storage.get('timeDisplay')
+    .then(timeDisplay => {
+        if(timeDisplay) {
+          console.log("Setting timeDisplay to ", timeDisplay);
+          this.timeDisplay = timeDisplay;
         } else {
-          this.autoRadius = 25;
+          this.timeDisplay = "24hr";
         }
+        this.storage.get('searchRange')
+        .then(searchValue => {
+            if(searchValue) {
+              this.autoRadius = searchValue;
+            } else {
+              this.autoRadius = 25;
+            }
+          this.storage.get('savedLat').then(value => {
+            if(value) {
+              console.log("mapLatitude was saved previously : ", value);
+              this.mapLatitude = value;
+              this.storage.get('savedLng').then(value => {
+                if(value) {
+                  console.log("mapLongitude was saved previously : ", value);
+                  this.mapLongitude = value;
+
+                  this.mapLatitude = parseFloat(this.mapLatitude);
+                  this.mapLongitude = parseFloat(this.mapLongitude);
+                  this.getMeetings();
+                } else {
+                  console.log("No mapLongitude previously saved");
+                }
+              });
+            } else {
+              console.log("No mapLatitude previously saved");
+            }
+          });
+        });
+      });
+
+      this.MapsAPIloader.load().then(() => {
+        console.log("MADE IT HERE");
+        let input = document.getElementById('autocompleteInput').getElementsByTagName('input')[0];
+
+        let autocomplete = new google.maps.places.Autocomplete(input, {});
+        google.maps.event.addListener(autocomplete, 'place_changed', () => {
+            let place = autocomplete.getPlace();
+            this.mapLatitude = place.geometry.location.lat();
+            this.mapLongitude = place.geometry.location.lng();
+            this.formattedAddress = place.formatted_address;
+            this.getMeetings();
+            console.log(place);
+        });
+      });
+  }
+
+
+  getMeetings(){
+    console.log("getMeetings:");
+    this.translate.get('LOADINGMAP').subscribe(value => {this.presentLoader(value);})
+
+    this.MeetingListProvider.getAutoRadiusMeetings(this.mapLatitude, this.mapLongitude, this.autoRadius).subscribe((data)=>{
+      console.log("getMeetings: subscribe data results");
+
+      if (JSON.stringify(data) == "{}") {  // empty result set!
+        console.log("getMeetings: empty result set");
+        this.meetingList = JSON.parse("[]");
+      } else {
+        console.log("getMeetings: non-empty result set", data);
+        this.meetingList  = data;
+        this.meetingList  = this.meetingList.filter(meeting => meeting.latitude = parseFloat(meeting.latitude));
+        this.meetingList  = this.meetingList.filter(meeting => meeting.longitude = parseFloat(meeting.longitude));
+        this.meetingList.filter(i => i.start_time = this.convertTo12Hr(i.start_time));
+
+      }
+
+      this.setLatLngOffsets();
+
+      this.dismissLoader();
     });
   }
 
-public getMeetings(){
-  console.log("Address Search : getMeetings");
-}
+  setLatLngOffsets() {
+    var i : any;
+    var dist : number = 0;
+    for (i = 0; i < this.meetingList.length - 1; i++) {
+      if (parseFloat(this.meetingList[i].distance_in_km) > dist) {
+        dist = parseFloat(this.meetingList[i].distance_in_km);
+      }
+      var longOffset : any = 0;
+      var latOffset  : any = 0;
+      var Offset     : any = 0.00002;
+      // maybe use :- https://github.com/TopicFriends/TopicFriends/commit/d6c61ae976eb1473b314bd804cebacd5106dac37
+      while ((this.meetingList[i].longitude == this.meetingList[i+1].longitude) &&
+             (this.meetingList[i].latitude == this.meetingList[i+1].latitude) ){
+        if ( (i % 2) === 1) {
+          longOffset += Offset;
+          this.meetingList[i].longitude = this.meetingList[i].longitude  + longOffset;
+        } else {
+          latOffset += Offset;
+          this.meetingList[i].latitude = this.meetingList[i].latitude  + latOffset;
+        }
+        i++;
+        if (i == (this.meetingList.length - 1)) {
+          longOffset = 0;
+          latOffset = 0;
+          break;
+        }
+      } // while
+
+    } // for
+
+    this.circleRadiusMeters = dist * 1000;
+  }
+
+  presentLoader(loaderText) {
+    if (!this.loader) {
+      this.loader = this.loadingCtrl.create({
+        content: loaderText
+      });
+      this.loader.present();
+    }
+  }
+
+  dismissLoader() {
+    if(this.loader){
+      this.loader.dismiss();
+      this.loader = null;
+    }
+  }
+
+  public convertTo12Hr(timeString){
+    if (this.timeDisplay == "12hr") {
+      var H = +timeString.substr(0, 2);
+      var h = H % 12 || 12;
+      var ampm = (H < 12 || H === 24) ? " AM" : " PM";
+      timeString = h + timeString.substr(2, 3) + ampm;
+      return timeString;
+    } else {
+     return timeString.slice(0, -3);
+    }
+  }
+
+  public radiusChange() {
+    var tempMapBounds = new google.maps.LatLngBounds();
+
+    this.circle.getBounds().then( value => {
+      console.log("GetBounds retuned!!", value);
+
+      tempMapBounds.extend(value.getNorthEast());
+      tempMapBounds.extend(value.getSouthWest());
+
+      this.mapBounds = tempMapBounds;
+    });
+
+  }
 
 }
--- src/pages/address-search/address-search.module.ts
+++ src/pages/address-search/address-search.module.ts
@@ -1,6 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SharedModule } from '../../app/shared/shared.module';
+import { NgModule }             from '@angular/core';
+import { CommonModule }         from '@angular/common';
+import { SharedModule }         from '../../app/shared/shared.module';
+import { AgmCoreModule }        from '@agm/core';
+import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
+import { Geolocation }          from '@ionic-native/geolocation';
+import { PipesModule }          from '../../pipes/pipes.module';
 
 import { AddressSearchComponent } from './address-search-component/address-search.component';
 
@@ -10,7 +14,13 @@ import { AddressSearchComponent } from './address-search-component/address-searc
   ],
   imports: [
     CommonModule,
-  	SharedModule
+  	SharedModule,
+    PipesModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAtwUjsIB14f0aHgdLk_JYnUrI0jvczMXw',
+      libraries: ['places']
+    }),
+    AgmJsMarkerClustererModule
     ],
     exports: [
       AddressSearchComponent
