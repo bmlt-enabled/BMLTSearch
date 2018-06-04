c1796f90d5d8efb416463d66422ab1c8cf02c0c5
Paul Nagle <paulnagle@gmail.com>
2018-06-04 17:59:57 +0100

    Removed unused plugins

--- re-plugin.sh
+++ re-plugin.sh
@@ -6,7 +6,6 @@ ionic cordova platform rm ios
 ionic cordova platform rm android
 ionic cordova platform rm browser
 
-ionic cordova plugin rm cordova-plugin-inappbrowser
 ionic cordova plugin rm cordova-plugin-splashscreen
 ionic cordova plugin rm cordova-plugin-statusbar
 ionic cordova plugin rm cordova-plugin-whitelist
@@ -16,7 +15,6 @@ ionic cordova platform add ios
 ionic cordova platform add android
 ionic cordova platform add browser
 
-ionic cordova plugin add cordova-plugin-inappbrowser
 ionic cordova plugin add cordova-plugin-splashscreen
 ionic cordova plugin add cordova-plugin-statusbar
 ionic cordova plugin add cordova-plugin-whitelist
