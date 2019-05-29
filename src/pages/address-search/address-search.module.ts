2fcd2fc021846077cf5e6870f592afc02b1af574
Paul Nagle <paulnagle@gmail.com>
2019-05-29 19:46:07 +0100

    Address search to use Google places API

--- src/pages/address-search/address-search.module.ts
+++ src/pages/address-search/address-search.module.ts
@@ -1,13 +1,11 @@
-import { NgModule }             from '@angular/core';
-import { CommonModule }         from '@angular/common';
-import { SharedModule }         from '../../app/shared/shared.module';
-import { AgmCoreModule }        from '@agm/core';
-import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
-import { Geolocation }          from '@ionic-native/geolocation';
-import { PipesModule }          from '../../pipes/pipes.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../app/shared/shared.module';
+import { Geolocation } from '@ionic-native/geolocation';
+import { PipesModule } from '../../pipes/pipes.module';
 
 import { AddressSearchComponent } from './address-search-component/address-search.component';
-import { ModalModule }          from "../modal/modal.module";
+import { ModalModule } from "../modal/modal.module";
 
 @NgModule({
   declarations: [
@@ -15,20 +13,15 @@ import { ModalModule }          from "../modal/modal.module";
   ],
   imports: [
     CommonModule,
-  	SharedModule,
+    SharedModule,
     PipesModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyAtwUjsIB14f0aHgdLk_JYnUrI0jvczMXw',
-      libraries: ['places']
-    }),
-    AgmJsMarkerClustererModule,
     ModalModule
-    ],
-    exports: [
-      AddressSearchComponent
-    ],
-    entryComponents:[
-      AddressSearchComponent
-    ]
+  ],
+  exports: [
+    AddressSearchComponent
+  ],
+  entryComponents: [
+    AddressSearchComponent
+  ]
 })
-export class AddressSearchModule {}
+export class AddressSearchModule { }
